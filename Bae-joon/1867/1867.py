e5485308dd95e95adc3b7df6c957d875556575bb
me <overelectric@gmail.com>
2019-12-22 22:23:05 +0900

    1867_remark

--- Bae-joon/1867/1867.py
+++ Bae-joon/1867/1867.py
@@ -19,22 +19,28 @@ def maximumMatching(matrix,line,biq,start) :
 		return False
 	find=matrix[line].index(True,start)
 	if(line==1) :
+	## 아예 처음줄을 검색할때는 무조건 넣을 수 있다.
 		biq[line]=find;
 	else :
 		if(find in biq) :
 			# if(search(matrix,line,biq)) :
 			temp_biq = [biq[i] for i in range(line)]
+			## 재배치를 하기 위해서 일단 배치 되어 있는 것을 백업한다,
 			for i in range(1, line):
 				if (True in matrix[i][biq[i] + 1:]):
 					# temp_biq[i]=matrix[i].index(True,biq[i]+1)
 					temp = matrix[i].index(True, biq[i] + 1)
+					## 현재 라인의 전에 있는 이분 배열에서 다른 곳으로 이어져 있는지 검사
 					if((temp in temp_biq)==False):
 						temp_biq[i] = temp
+					## 이어져 있는게 다른 곳과 이어져 있지 않다면 임시 배치에 넣는다.
 					if ((find in temp_biq)==False):
+					## 없다면 다시 넣는다.
 						biq[line] = find
 						for i in range(1, line):
 							if (temp_biq[i] != 0):
 								biq[i] = temp_biq[i]
+					
 
 				else:
 					if (i == line - 1):
