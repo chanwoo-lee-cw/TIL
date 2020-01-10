fcc52b7c8e3f65347e1d266c4feb9bf3b287f58c
Chanwoo <overelectric@gmail.com>
2020-01-10 17:48:35 +0900

    1149-e

--- Bae-joon/1149/1149.py
+++ Bae-joon/1149/1149.py
@@ -2,24 +2,22 @@ import sys
 input = sys.stdin.readline
 sys.setrecursionlimit(1000000000)
 
-
 dp = []
 
-def meno(color, pos, coloring,n) :
+def memo(color, pos, coloring, n) :
     if(pos == n-1) :
         return color[pos][coloring]
     if(dp[pos][coloring]!=0) :
         return dp[pos][coloring]
     if(coloring==0) :
-        dp[pos][coloring] = min(meno(color, pos + 1, 1, n), meno(color, pos + 1, 2, n)) + color[pos][coloring]
+        dp[pos][coloring] = min(memo(color, pos + 1, 1, n), memo(color, pos + 1, 2, n)) + color[pos][coloring]
         return dp[pos][coloring]
     elif (coloring==1) :
-        dp[pos][coloring] = min(meno(color, pos + 1, 0, n), meno(color, pos + 1, 2, n)) + color[pos][coloring]
+        dp[pos][coloring] = min(memo(color, pos + 1, 0, n), memo(color, pos + 1, 2, n)) + color[pos][coloring]
         return dp[pos][coloring]
     else :
-        dp[pos][coloring] = min(meno(color, pos + 1, 0, n), meno(color, pos + 1, 1, n)) + color[pos][coloring]
+        dp[pos][coloring] = min(memo(color, pos + 1, 0, n), memo(color, pos + 1, 1, n)) + color[pos][coloring]
         return dp[pos][coloring]
-
 
 if __name__ == "__main__" :
     n = int(input().strip())
@@ -31,4 +29,4 @@ if __name__ == "__main__" :
         color.append(temp)
     dp = [[0 for _ in range(3)]for _ in range(n)]
 
-    print(min(meno(color, 0, 0, n), meno(color, 0, 1, n), meno(color, 0, 2, n)))
+    print(min(memo(color, 0, 0, n), memo(color, 0, 1, n), memo(color, 0, 2, n)))
