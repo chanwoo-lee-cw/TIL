76a9014dcb1083b28d02c1b1a6ee2717784c28d4
Chanwoo <overelectric@gmail.com>
2020-02-12 16:03:41 +0900

    2020-02-12

--- Spring/springedu/src/main/java/my/spring/springedu/UploadController2.java
+++ Spring/springedu/src/main/java/my/spring/springedu/UploadController2.java
@@ -23,8 +23,13 @@ public class UploadController2 {
 
 	@RequestMapping("/upload2")
 	public ModelAndView saveFile(MultipartRequest mreq) {
+		//MultipartRequest를 사용하면 전달되는 파일이 1개가 아니라 여러개라도 된다.
+		// 매개변수를 multifile로 해야한다.
+		// 만약 Request로 한다면 파일을 직접 꺼내줘야 한다.
+		// 그게 바로 밑에 getFiles이다.
 		ModelAndView mav = new ModelAndView();
 		List<MultipartFile> list = mreq.getFiles("mfile");
+		// 리스트즉, 업로드 되는 파일 갯수만큼 파일이 들어간다.
 		String resultStr = "";
 		String path = "c:/uploadtest/multi";
 		File isDir = new File(path);
@@ -33,6 +38,7 @@ public class UploadController2 {
 		}
 		mav.setViewName("uploadForm2");
 		for (MultipartFile mfile : list) {
+			// 파일의 갯수만큼 반복문
 			String fileName = mfile.getOriginalFilename();
 			try {
 				File f = new File("c:/uploadtest/multi/" + fileName);
@@ -42,6 +48,8 @@ public class UploadController2 {
 					resultStr += fileName + " : 파일이 이미 존재해요!!<br>";
 				} else {
 					mfile.transferTo(f);
+					// 파일 아웃 풋을 사용하지 않고, 멀티파트의 매서드를 사용해서 올리는 형태
+					// 받아와서 추가적인 액션을 전혀 하지 않고 바로 내보내는 형태이다.
 					resultStr += fileName + " : 파일이 저장되었어요!!<br>";
 				}
 			} catch (IOException e) {
