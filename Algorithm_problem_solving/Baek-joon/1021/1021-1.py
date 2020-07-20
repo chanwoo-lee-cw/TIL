f5564580cdc583da53d37e2434b2e1ec897cf138
overmaind <overelectric@gmail.com>
2020-07-20 17:11:52 +0900

    Add 1021.py

---
+++ Algorithm_problem_solving/Baek-joon/1021/1021-1.py
@@ -0,0 +1,22 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n, m = list(map(int, input().strip().split()))
+want = list(map(int, input().strip().split()))
+deq = deque(range(1, n+1))
+
+answer = 0
+for num in want:
+    idx = deq.index(num)
+
+    if idx <= len(deq) - idx:
+        deq.rotate(-idx)
+        answer += idx
+    else:
+        deq.rotate(len(deq) - idx)
+        answer += (len(deq)-idx)
+    if num == deq[0]:
+        deq.popleft()
+
+print(answer)
