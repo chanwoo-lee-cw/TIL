f605b58018b82910d827b8c2e31ccd9882edb2f5
overmaind <overelectric@gmail.com>
2020-07-26 03:54:35 +0900

    Add net-ch05.md

--- Lecture/컴퓨터네트워크/Ch05/TCP_echo_server.c
+++ Lecture/컴퓨터네트워크/Ch05/TCP_echo_server.c
@@ -1,4 +1,46 @@
+#include "unp.h"
+
+void str_echo(int sockfd)
+{
+    ssize_t n;
+    char buf[MAXLINE]
+
+    again:
+    while ((n = read(sockfd, buf, MAXLINE)) > 0)
+    {
+        Writen(sockfd, buf, n);
+    }
+    if (n < 0 && errno == EINTR)
+        goto again;
+    else if (n < 0)
+        err_sys("str_echo: read error");
+}
+
 int main(int srgc, char **argv)
 {
-       
+    int listenfd, connfd;
+    pid_t childpid;
+    socklen_t childlen;
+    struct sockaddr_in cliaddr, servaddr;
+
+    listenfd = Socket(AF_INEF, SOCK_STREAM, 0);
+
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_familly = AF_INET;
+    servaddr.sin_addr.s_adrr = htonl(INADDR_ANY);
+
+    Bind(listenfd, LISTENQ);
+
+    for(;;) {
+        clilen = sizeof(cliaddr);
+        connfd = Accept(listenfd, (SA *) &cliaddr, &cliaddr);
+
+        if( (childpid = Fork()) == 0)   // child process
+        {
+            Close(listenfd);    // 리스팅 소켓을 닫는다.
+            str_echo(connfd);   // 리퀘스트를 진행한다.
+            exit();
+        }
+        Close(connfd);  // 부모의 연결 소켓을 닫는다.
+    }
 }
