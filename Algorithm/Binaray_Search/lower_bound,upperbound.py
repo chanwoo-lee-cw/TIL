59cc0d84d91c958a24f3e9037abba006dea9e1c7
overmaind <overelectric@gmail.com>
2020-08-22 14:01:19 +0900

    update lower,upper_bound.py

--- Algorithm/Binaray_Search/lower_bound,upperbound.py
+++ Algorithm/Binaray_Search/lower_bound,upperbound.py
@@ -1,19 +1,37 @@
-def lower_bound(arr, start, end,  check) :
-    while end - start > 0 :
-        mid = (start + end) // 2
-        if arr[mid] < check :
-            start = mid + 1
-        else :
+def lower_bound(start, end, arr, find):
+    while end - start > 0:
+        mid = (end + start)//2
+        if arr[mid] < find:
+            start = mid+1
+        else:
             end = mid
-    return end+1
-    # return end
+    return (end + start)//2 + 1
 
-def upper_bound(arr, start, end,  check) :
-    while end - start > 0 :
-        mid = (start + end) // 2
-        if arr[mid] <= check :
-            start = mid + 1
-        else :
+# def lower_bound(arr, start, end,  check) :
+#     while end - start > 0 :
+#         mid = (start + end) // 2
+#         if arr[mid] < check :
+#             start = mid + 1
+#         else :
+#             end = mid
+#     return end+1
+#     # return end
+
+def upper_bound(start, end, arr, find):
+    while end - start > 0:
+        mid = (end + start)//2
+        if arr[mid] <= find:
+            start = mid+1
+        else:
             end = mid
-    return end+1
-    # return end
+    return (end + start)//2 + 1
+
+# def upper_bound(arr, start, end,  check) :
+#     while end - start > 0 :
+#         mid = (start + end) // 2
+#         if arr[mid] <= check :
+#             start = mid + 1
+#         else :
+#             end = mid
+#     return end+1
+#     # return end
