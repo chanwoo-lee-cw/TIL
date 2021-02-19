75721e4c086ebc911627f6a8c7588b188584c0ea
Chanwoo <overelectric@gmail.com>
2021-02-19 20:24:35 +0900

    Add Java1562-F.java

--- Algorithm_problem_solving/Baek-joon/1562/Java1562.java
+++ Algorithm_problem_solving/Baek-joon/1562/Java1562.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.HashSet;
 
 public class Main {
@@ -24,10 +25,17 @@ public class Main {
 class StiarNumToN {
     int n;
     boolean[] numUsed;
+    long[][][] dp;
 
     public StiarNumToN(int n) {
         this.n = n;
         numUsed = new boolean[10];
+        dp = new long[n + 1][10][1 << 10];
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < 10; j++) {
+                Arrays.fill(dp[i][j], -1);
+            }
+        }
     }
 
     public long getStiarNumToN() {
@@ -40,6 +48,10 @@ class StiarNumToN {
 
     public long makeStairNum(int digits, int lastNum) {
         long answer = 0;
+        int currBinary = getbinary();
+        if (dp[digits][lastNum][currBinary] != -1) {
+            return dp[digits][lastNum][getbinary()];
+        }
         numUsed[lastNum] = true;
         if (n == digits) {
             for (int i = 0; i < 10; i++) {
@@ -59,7 +71,17 @@ class StiarNumToN {
             }
         }
         numUsed[lastNum] = false;
+        dp[digits][lastNum][getbinary()] = answer;
         return answer;
     }
 
+    public int getbinary() {
+        int answer = 0;
+        for (int i = 0; i < 10; i++) {
+            answer = answer << 1;
+            if (numUsed[i])
+                answer++;
+        }
+        return answer;
+    }
 }
