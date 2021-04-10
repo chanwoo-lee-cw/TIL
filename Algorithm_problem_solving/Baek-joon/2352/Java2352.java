be490d81e680139e1d6cc2bbb5c2d6fbe3a960aa
Chanwoo <overelectric@gmail.com>
2021-04-10 20:29:36 +0900

    Temp Java1509.java

--- Algorithm_problem_solving/Baek-joon/2352/Java2352.java
+++ Algorithm_problem_solving/Baek-joon/2352/Java2352.java
@@ -6,7 +6,7 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) {
         Semiconductor semiconductor;    // 반도체 객체
-        int n;  // 반도
+        int n;  // 반도체 포트의 갯수
         String input;
         try {
             BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
@@ -29,14 +29,17 @@ public class Main {
 lis 문제
  */
 class Semiconductor {
-    private int n;
-    private int[] linkablePort;
+    private int n;      // 반도체 포트의 갯수
+    private int[] linkablePort; // linkablePort[i]가 연결할 수 있는 포트 번호
 
     public Semiconductor(int n) {
         this.n = n;
         this.linkablePort = new int[n + 1];
     }
 
+    /*
+    입력된 문자열을 통해 포트 번호 연결
+    */
     public void setLinkablePort(String input) {
         StringTokenizer st = new StringTokenizer(input);
         for (int i = 1; i < n + 1; i++) {
@@ -44,6 +47,9 @@ class Semiconductor {
         }
     }
 
+    /*
+    lis를 위한 lowerbound;
+    */
     private int lowerBound(int s, int e, int[] arr, int find) {
         int m;
         while (e - s > 0) {
@@ -57,10 +63,13 @@ class Semiconductor {
         return (s + e) / 2;
     }
 
+    /*
+    최대로 연결 할 수 있는 포트 번호
+    */
     public int maxLink() {
-        int[] lis = new int[n];
+        int[] lis = new int[n];     // lis를 위한 배열
+        int len = 1;                // lis의 현재 길이
         lis[0] = linkablePort[1];
-        int len = 1;
 
         for (int i = 2; i < n + 1; i++) {
             if (linkablePort[i] > lis[len - 1])
