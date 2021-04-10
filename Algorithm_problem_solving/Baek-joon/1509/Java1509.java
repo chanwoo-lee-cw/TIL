7a09efd46c2325ff7b4907fbea622f565e942dfb
Chanwoo <overelectric@gmail.com>
2021-04-10 20:42:09 +0900

    Java 1509 - Add dfs에 dp 추가

--- Algorithm_problem_solving/Baek-joon/1509/Java1509.java
+++ Algorithm_problem_solving/Baek-joon/1509/Java1509.java
@@ -9,9 +9,10 @@ public class Main {
         try {
             BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
             input = bf.readLine();
+
             palindrome = new Palindrome(input);
             palindrome.makeDP();
-            System.out.println(palindrome.getMinPalindrome(1));
+            System.out.println(palindrome.getMinPalindrome());
 
             bf.close();
         } catch (IOException e) {
@@ -26,6 +27,7 @@ class Palindrome {
     private char[] sentence;    // 입력된 문장
     private int n;      // 팰린드롬 문장의 길이
     private boolean[][] dp;     // dp[i][j] - setence i~j의 팰린드롬 여부
+    private int[] minSplit;     // minSplit[i] - i~n까지의 문장의 가장 적은 수로 분할 할 수 있는 팰린드롬
 
     public Palindrome(String sentence) {
         this.sentence = new char[sentence.length() + 1];
@@ -53,16 +55,26 @@ class Palindrome {
     }
 
     /*
-    dfs방식으로 최소로 나눌 수 있는 경우의 수를 리턴
+    s부터 시작 되는 문장에서 dfs방식으로 최소로 나눌 수 있는 경우의 수를 리턴
     */
     public int getMinPalindrome(int s) {
         if (s > n)
             return 0;
+        else if (minSplit[s] != 0)
+            return minSplit[s];
         int answer = Integer.MAX_VALUE;
         for (int i = s; i <= n; i++) {
             if (dp[s][i])
                 answer = Math.min(answer, getMinPalindrome(i + 1) + 1);
         }
         return answer;
+    }
+
+    /*
+    dfs 방식으로 전체 문장에서 가장 적은 수로 나눌 수 있는 팰린드롬을 리턴
+    */
+    public int getMinPalindrome() {
+        minSplit = new int[n + 1];
+        return getMinPalindrome(1);
     }
 }
