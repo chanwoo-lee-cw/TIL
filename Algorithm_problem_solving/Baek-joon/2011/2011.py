1cda38ab8d53c59038e4a531f62b4ec255bcdde9
Chanwoo <overelectric@gmail.com>
2021-06-19 22:12:28 +0900

    Add 2011.py

---
+++ Algorithm_problem_solving/Baek-joon/2011/2011.py
@@ -0,0 +1,28 @@
+from sys import stdin
+
+input = stdin.readline
+
+
+def cypherNumCase(cypher):
+    cypherLen = len(cypher)
+    dp = [0] * (cypherLen + 1)
+    # basecase
+    dp[0] = 1
+    dp[1] = 1 if cypher[-1] != '0' else 0
+    # 점화식
+    for i in range(2, cypherLen + 1):
+        if cypher[-i] == '0' and cypher[-(i - 1)] == '0':
+            return 0
+        if cypher[-i] == '1':
+            dp[i] += dp[i - 2]
+        elif cypher[-i] == '2' and int(cypher[-(i - 1)]) <= 6:
+            dp[i] += dp[i - 2]
+        if cypher[-(i - 1)] != '0':
+            dp[i] += dp[i - 1]
+        dp[i] %= 1000000
+    return dp[cypherLen]
+
+
+if __name__ == "__main__":
+    cypher = input().strip()
+    print(cypherNumCase(cypher))
