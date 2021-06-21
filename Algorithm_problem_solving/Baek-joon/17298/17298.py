479a030528b2758426265b1243d347f090ee71f9
Chanwoo <overelectric@gmail.com>
2021-06-21 17:39:02 +0900

    Add 17298.py

--- Algorithm_problem_solving/Baek-joon/17298/17298.py
+++ Algorithm_problem_solving/Baek-joon/17298/17298.py
@@ -2,16 +2,18 @@ from sys import stdin
 
 input = stdin.readline
 
-
+"""
+arr배열의 오큰수 배열을 리턴한다.
+"""
 def findFisrtGreater(n, arr):
-    stk = []
-    answer = [-1] * n
+    stk = []    # 오큰술ㄹ 저장할 배열
+    answer = [-1] * n       # 각각 위치의 오큰수의 배열
     stk.append((arr[0], 0))
     for i in range(1, n):
         if (stk[-1][0]) >= arr[i]:
             stk.append((arr[i], i))
         else:
-            while stk and stk[-1][0] <= arr[i]:
+            while stk and stk[-1][0] < arr[i]:
                 curr = stk.pop()
             stk.append((arr[i], i))
             for j in range(curr[1], i):
