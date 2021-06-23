86fd9f542b8fcc82c98fd9a5b6ea8db694d5a0b9
Chanwoo <overelectric@gmail.com>
2021-06-23 17:46:19 +0900

    update 17425.py

--- Algorithm_problem_solving/Baek-joon/17425/17425.py
+++ Algorithm_problem_solving/Baek-joon/17425/17425.py
@@ -4,22 +4,23 @@ input = stdin.readline
 
 if __name__ == "__main__":
     t = int(input())
+    MAX = 1000000
     tcase = []
     for i in range(t):
         tcase.append(int(input()))
     maxNum = max(tcase)
 
-    fx = [0] * (maxNum + 1)
+    fx = [1] * (maxNum + 1)
     gx = [0] * (maxNum + 1)
 
+    for i in range(2, maxNum + 1):
+        for j in range(1, MAX + 1):
+            if i * j > maxNum:
+                break
+            fx[i * j] += i
+
     for i in range(1, maxNum + 1):
-        numSqrt = int(i ** 0.5)
-        for j in range(1, numSqrt + 1):
-            if i % j == 0:
-                fx[i] += j
-                if i // j != j:
-                    fx[i] += i // j
-        gx[i] += gx[i - 1] + fx[i]
+        gx[i] = gx[i - 1] + fx[i]
 
     for item in tcase:
         print(gx[item])
