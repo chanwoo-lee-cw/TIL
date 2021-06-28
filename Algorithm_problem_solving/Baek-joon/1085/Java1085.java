2067a729b7bbd2b80f5e74ef7076042673f489ba
Chanwoo <overelectric@gmail.com>
2021-06-28 19:48:49 +0900

    update Java1085.java

--- Algorithm_problem_solving/Baek-joon/1085/Java1085.java
+++ Algorithm_problem_solving/Baek-joon/1085/Java1085.java
@@ -5,9 +5,9 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) {
-        int x, y;
-        int w, h;
-        int answer;
+        int x, y;   // 현재 위치
+        int w, h;   // 직사각형의 크기
+        int answer; // 출력될 정답
         try {
             BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
             StringTokenizer st = new StringTokenizer(bf.readLine());
@@ -16,7 +16,7 @@ public class Main {
             w = Integer.parseInt(st.nextToken());
             h = Integer.parseInt(st.nextToken());
 
-            answer = Math.min(Math.min(x, y), Math.min(w - x, h - y));
+            answer = Math.min(Math.min(x, y), Math.min(w - x, h - y));  // 직선 방향으로 가장 가까운 모서리 찾기
             System.out.println(answer);
         } catch (IOException e) {
             e.printStackTrace();
