3935823c1d335e0e63418886c82a00e04bf2b3d5
Chanwoo <overelectric@gmail.com>
2021-07-17 16:39:48 +0900

    update 1789.java

--- Algorithm_problem_solving/Baek-joon/1712/1712.py
+++ Algorithm_problem_solving/Baek-joon/1712/1712.py
@@ -4,12 +4,16 @@ from sys import stdin
 
 input = stdin.readline
 
-
+"""
+손익분기점을 반환한다.
+"""
 def getBreakEvenPoint(a, b, c):
     if c <= b:
+        # 생산비가 이윤보다 크다면 -1
         return -1
     cnt = a / (c - b)
     answer = math.ceil(cnt)
+    # 이윤이 생기는 시점이기 때문에 정확히 손익이 일치한다면 +1을 해서 리턴
     return answer + 1 if answer == math.floor(cnt) else answer
 
 
--- Algorithm_problem_solving/Baek-joon/1789/Java1789.java
+++ Algorithm_problem_solving/Baek-joon/1789/Java1789.java
@@ -5,9 +5,9 @@ import java.io.InputStreamReader;
 public class Main {
 
     public static void main(String[] args) {
-        long n;
-        long sum;
-        long answer = 0;
+        long n;     // 입력된 숫자
+        long sum;   // 1~i까지 전부 더했을 때의 합
+        long answer = 0;    // 정답을 저장하는 변수
         try {
             BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
             n = Long.parseLong(bf.readLine());
@@ -18,6 +18,7 @@ public class Main {
                 if (sum > n) {
                     break;
                 }
+                // 만약 현재 다 더한 합이 더 맞다면, 그 전단계에서 ++를 하면 되기 때문에 그 전 단계의 값을 저장한다.
                 answer = i;
             }
         } catch (IOException error) {
