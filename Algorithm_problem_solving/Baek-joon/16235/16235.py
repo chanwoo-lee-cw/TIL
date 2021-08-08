76d8f096eb4eabc1d10880739276a59000e4bbd2
Chanwoo <overelectric@gmail.com>
2021-08-09 00:00:50 +0900

    comple 16235.py

--- Algorithm_problem_solving/Baek-joon/16235/16235.py
+++ Algorithm_problem_solving/Baek-joon/16235/16235.py
@@ -1,5 +1,5 @@
 import sys
-import heapq
+from collections import deque
 
 input = sys.stdin.readline
 
@@ -8,26 +8,28 @@ spred = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1))
 """
 해당 나무 리스트에 한 해에 처리되는 경우
 """
-def oneYearAgo(trees, arc):
-    prduction = {}      # 번식 할 나무의 수
+def oneYearAgo(trees, arc, treeCnt):
+    prduction = {}  # 번식 할 나무의 수
     for item in trees:
         # 봄
         area = trees[item]  # (item[0], item[1]) 자리에 심어 있는 나무 리스트
-        reIn = []   # 영양을 빨아드린 나무
-        dieTree = 0     # 죽은 나무 -> 즉 다시 영양분으로 바"뀔 수치
+        reIn = deque()  # 영양을 빨아드린 나무
+        dieTree = 0  # 죽은 나무 -> 즉 다시 영양분으로 바뀔 수치
         while area:
-            tree = heapq.heappop(area)
+            tree = area.popleft()
             if arc[item[0]][item[1]] >= tree:
                 arc[item[0]][item[1]] -= tree
-                heapq.heappush(reIn, tree + 1)
+                reIn.append(tree + 1)
+                # reIn.append(tree + 1)
                 if (tree + 1) % 5 == 0:
                     if not (item[0], item[1]) in prduction:
                         prduction[(item[0], item[1])] = 1
                     else:
                         prduction[(item[0], item[1])] += 1
             else:
+                treeCnt -= 1
                 dieTree += tree // 2
-        trees[item] = reIn
+        trees[item] = reIn  # 해당 좌표에 현재 살아 있는 나무를 다시 대입
         # 여름
         arc[item[0]][item[1]] += dieTree
     # 가을
@@ -37,15 +39,18 @@ def oneYearAgo(trees, arc):
             nextX = next[1] + item[1]
             if nextY <= 0 or nextX <= 0 or nextY > n or nextX > n:
                 continue
+            # else
+            treeCnt += prduction[item]
+            if not (nextY, nextX) in trees:
+                trees[(nextY, nextX)] = deque([])
             for _ in range(prduction[item]):
-                if not (nextY, nextX) in trees:
-                    trees[(nextY, nextX)] = [1]
-                else:
-                    heapq.heappush(trees[(nextY, nextX)], 1)
+                trees[(nextY, nextX)].appendleft(1)
     # 겨울
     for i in range(n):
         for j in range(n):
-            arc[i + 1][j + 1] += a[i + 1][j + 1]
+            arc[i + 1][j + 1] += a[j + 1][i + 1]
+
+    return treeCnt
 
 
 if __name__ == "__main__":
@@ -53,6 +58,9 @@ if __name__ == "__main__":
     a = [[0] * (n + 1) for _ in range(n + 1)]
     arc = [[5] * (n + 1) for _ in range(n + 1)]
     trees = {}
+    treeCnt = 0
+    arr = []
+
     for i in range(n):
         inputLine = list(map(int, input().strip().split()))
         for j in range(n):
@@ -60,14 +68,16 @@ if __name__ == "__main__":
     inputLine = None
     for _ in range(m):
         x, y, age = map(int, input().strip().split())
+        treeCnt += 1
         if not (y, x) in trees:
             trees[(y, x)] = [age]
         else:
-            heapq.heappush(trees[(y, x)], age)
+            trees[(y, x)].append(age)
+    for item in trees:
+        trees.get(item).sort()
+        trees[item] = deque(trees.get(item))
 
     for _ in range(k):
-        oneYearAgo(trees, arc)
-    treeCnt = 0
-    for item in trees:
-        treeCnt += len(trees[item])
+        treeCnt = oneYearAgo(trees, arc, treeCnt)
+
     print(treeCnt)
