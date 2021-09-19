c9fc3d7737688d9892e24b1ca8b8bef8bec8e177
Chanwoo <overelectric@gmail.com>
2021-09-19 17:18:59 +0900

    Add 2447.java

--- Algorithm_problem_solving/Baek-joon/2447/2447.py
+++ Algorithm_problem_solving/Baek-joon/2447/2447.py
@@ -1,5 +1,6 @@
 import sys
-sys.setrecursionlimit(10**5)
+
+sys.setrecursionlimit(10 ** 5)
 input = sys.stdin.readline
 
 
@@ -9,26 +10,26 @@ def divide(xs, xe, ys, ye):
         return
 
     x_line = [xs]
-    x_line.append(xs + (xe - xs)//3)
-    x_line.append(xs + (xe - xs)//3 * 2)
+    x_line.append(xs + (xe - xs) // 3)
+    x_line.append(xs + (xe - xs) // 3 * 2)
     x_line.append(xe)
 
     y_line = [ys]
-    y_line.append(ys + (ye - ys)//3)
-    y_line.append(ys + (ye - ys)//3 * 2)
+    y_line.append(ys + (ye - ys) // 3)
+    y_line.append(ys + (ye - ys) // 3 * 2)
     y_line.append(ye)
 
     for i in range(3):
         for j in range(3):
             if i == 1 and j == 1:
                 continue
-            divide(x_line[j], x_line[j+1], y_line[i], y_line[i+1])
+            divide(x_line[j], x_line[j + 1], y_line[i], y_line[i + 1])
 
 
 if __name__ == "__main__":
     N = int(input().strip())
 
-    star = [[' ']*N for _ in range(N)]
+    star = [[' '] * N for _ in range(N)]
 
     divide(0, N, 0, N)
 
