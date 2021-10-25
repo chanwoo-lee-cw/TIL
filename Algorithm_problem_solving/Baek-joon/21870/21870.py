54f622bf548835234a69c84e7f458137374c4c8d
Chanwoo <overelectric@gmail.com>
2021-10-26 02:41:41 +0900

    Updata 21870.py(맞았습니다- 틀렸던 이유 : 우측은 올림을 바탕으로 계산)

--- Algorithm_problem_solving/Baek-joon/21870/21870.py
+++ Algorithm_problem_solving/Baek-joon/21870/21870.py
@@ -2,7 +2,9 @@ from sys import stdin
 
 input = stdin.readline
 
-
+"""
+gcd(a, b) - a와 b의 최대공약수를 계산한다.
+"""
 def gcd(a, b):
     if b > a:
         a, b = b, a
@@ -12,21 +14,23 @@ def gcd(a, b):
     return a
 
 
+"""
+getMaxGcd(n, arr) - n길이의 배열 arr에서의 답을 구한다.
+"""
 def getMaxGcd(n, arr):
     if n == 1:
         return arr[0]
     m = n // 2
-    leftAnswer = 0
+    # 좌측에서 GCD를 계산하는 경우
     left = arr[0]
     for i in range(1, m):
         left = gcd(left, arr[i])
     leftAnswer = left + getMaxGcd(n - m, arr[m:])
-
-    rightAnswer = 0
+    # 우측에서 GCD를 계산하는 경우
     right = arr[-1]
-    for i in range(1, m):
+    for i in range(1, n - m):
         right = gcd(right, arr[-(i + 1)])
-    rightAnswer = right + getMaxGcd(n - m, arr[:n - m])
+    rightAnswer = right + getMaxGcd(m, arr[:-(n - m)])
 
     return max(leftAnswer, rightAnswer)
 
