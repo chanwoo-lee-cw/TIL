4f1a0d118f22ab38611646efa2f07a8527e615cf
Chanwoo <overelectric@gmail.com>
2021-10-30 04:31:52 +0900

    Add 6137-F.py(틀렸습니다)

---
+++ Algorithm_problem_solving/Baek-joon/6137/6137-F.py
@@ -0,0 +1,25 @@
+from sys import stdin
+
+input = stdin.readline
+
+if __name__ == "__main__":
+    n = int(input().strip())
+    s = []        # 입력된 문자열
+    t = []        # 완성된 문자열
+    for i in range(n):
+        s.append(input().strip())
+    cnt = 0
+    while s:
+        if ord(s[0]) < ord(s[-1]):
+            # 앞의 글자가 사전순으로 빠르면 앞에서 pop
+            t.append(s.pop(0))
+        else:
+            # 뒤의 글자가 사전순으로 빠르면 뒤에서 pop
+            t.append(s.pop(-1))
+        cnt += 1
+        if cnt == 80:
+            # 문자가 80개가 될 때마다 '\n' 추가
+            t.append("\n")
+            cnt = 0
+    print(''.join(t))
+
