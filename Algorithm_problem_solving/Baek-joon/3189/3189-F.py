b88ae78ac638ba6949d52437db5d646cfebd0875
Chanwoo <overelectric@gmail.com>
2023-01-22 23:48:37 +0900

    feature/ update 3189.py

--- Algorithm_problem_solving/Baek-joon/3189/3189-F.py
+++ Algorithm_problem_solving/Baek-joon/3189/3189-F.py
@@ -6,21 +6,23 @@ if __name__ == "__main__":
     a, b, c = map(int, input().strip().split())
 
     suffix_len = len(str(c))
-    curr, answers = a, [str(a)]
-    i = 0
-    while True:
-        i += 1
-        curr *= b
-        answers.append(str(curr))
-        if len(str(curr)) <= suffix_len:
-            if curr == c:
-                print(i)
-                break
-        else:
-            temp = int(str(curr)[-suffix_len:])
-            if temp == c:
-                print(i)
-                break
-            if i != 1 and str(curr)[-len(answers[1]):] == answers[1]:
-                print("NIKAD")
-                break
+    curr, first = a, str(a * b)
+    if b <= 1 or a <= 0:
+        print("NIKAD")
+    else:
+        i = 0
+        while True:
+            i += 1
+            curr *= b
+            if len(str(curr)) <= suffix_len:
+                if curr == c:
+                    print(i)
+                    break
+            else:
+                temp = int(str(curr)[-suffix_len:])
+                if temp == c:
+                    print(i)
+                    break
+                if i != 1 and str(curr)[-len(first):] == first:
+                    print("NIKAD")
+                    break
