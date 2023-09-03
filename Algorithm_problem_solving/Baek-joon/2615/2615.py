e6db5998a9ea1c540b3feab14537f047a485fcbe
Chanwoo <overelectric@gmail.com>
2023-09-03 22:32:20 +0900

    update 2615.py

--- Algorithm_problem_solving/Baek-joon/2615/2615.py
+++ Algorithm_problem_solving/Baek-joon/2615/2615.py
@@ -19,10 +19,15 @@ class Omock:
         """
         for i in range(SIZE):
             for j in range(SIZE):
-                if matrix[i][j] != 0:
+                if self.matrix[i][j] != 0:
                     for next in WAY:
-                        if self.check_result(next, i, j, 1, matrix[i][j]):
-                            return matrix[i][j], (i + 1, j + 1)
+                        pre_y = i - next[0]
+                        pre_x = j - next[1]
+                        if 0 <= pre_y < SIZE and 0 <= pre_x < SIZE:
+                            if self.matrix[i][j] == self.matrix[pre_y][pre_x]:
+                                continue
+                        if self.check_result(next, i, j, 1, self.matrix[i][j]):
+                            return self.matrix[i][j], (i + 1, j + 1)
         return 0, (0, 0)
 
     def check_result(self, next, y, x, count, win):
@@ -37,19 +42,18 @@ class Omock:
         """
         next_y = next[0] + y
         next_x = next[1] + x
-        if next_y >= SIZE or next_x >= SIZE or next_x < 0 or next_y < 0:
-            return False
-        elif self.matrix[next_y][next_x] != win:
-            return False
-        else:
-            count += 1
-            if count >= 5:
-                return True
+        if count < 5:
+            if self.matrix[next_y][next_x] != win and next_y >= SIZE or next_x >= SIZE or next_x < 0 or next_y < 0:
+                return False
+        elif count == 5:
+            if self.matrix[next_y][next_x] == win:
+                return False
             else:
-                return self.check_result(next, next_y, next_x, count, win)
+                return True
+        return self.check_result(next, next_y, next_x, count + 1, win)
 
 
-if __name__ == "__main__":
+def solution():
     matrix = []
     for item in range(SIZE):
         matrix.append(list(map(int, input().strip().split())))
@@ -59,3 +63,7 @@ if __name__ == "__main__":
         print(0)
     else:
         print(f"{win}\n{position[0]} {position[1]}")
+
+
+if __name__ == "__main__":
+    solution()
