44809143155889a54f037eaf8c88198b504e519b
Chanwoo <overelectric@gmail.com>
2023-09-30 14:58:54 +0900

    Add 오랜 기간 보호한 동물(2).sql

---
+++ Algorithm_problem_solving/Programers/SQL/오랜 기간 보호한 동물(2)/오랜 기간 보호한 동물(2).sql
@@ -0,0 +1,9 @@
+-- 코드를 입력하세요
+SELECT result.ANIMAL_ID, result.NAME
+FROM (  
+    SELECT ins.ANIMAL_ID, ins.NAME, (outs.DATETIME - ins.DATETIME) AS STAY_DATES
+    FROM ANIMAL_INS AS ins INNER JOIN ANIMAL_OUTS AS outs
+    ON ins.ANIMAL_ID = outs.ANIMAL_ID
+    ORDER BY STAY_DATES DESC
+    LIMIT 0, 2
+) AS result
