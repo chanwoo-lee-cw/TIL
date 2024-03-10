3fff2df93b018c3420afc98024d1efdf381c601d
Chanwoo <overelectric@gmail.com>
2024-03-10 22:03:46 +0900

    Update 1546.kt

---
+++ Algorithm_problem_solving/Baek-joon/1546/1546.kt
@@ -0,0 +1,33 @@
+import java.io.IOException
+
+fun main() {
+    val n: Int = readlnOrNull()?.toIntOrNull() ?: throw IOException("에러 발생")
+    val grades = readln().trim().split(" ").map { it.toFloat() }.toTypedArray()
+
+    val grade = Grade(grades)
+    grade.convertGrade()
+    println(grade.average)
+}
+
+class Grade(
+    val grades: Array<Float> = emptyArray()
+) {
+    init {
+        if (grades.isEmpty()) throw IOException("에러 발생")
+    }
+
+    val n: Int = grades.size
+    val maxGrade: Float
+        get() = if (n != 0) grades.max() else 0f
+
+    val average: Float
+        get() = if (n != 0) grades.sum() / n else 0f
+
+    fun convertGrade(): Unit {
+        val maxScore = maxGrade
+
+        for (i in 0..<n) {
+            grades[i] = grades[i] * 100 / maxScore
+        }
+    }
+}
