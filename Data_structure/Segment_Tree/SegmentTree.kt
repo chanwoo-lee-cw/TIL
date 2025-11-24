541a25429582cab15a873a96c99b5cc63d03ba18
Chanwoo <overelectric@gmail.com>
2025-11-24 23:58:34 +0900

    Add SegmentTree.kt

---
+++ Data_structure/Segment_Tree/SegmentTree.kt
@@ -0,0 +1,97 @@
+class SegmentTree(
+    val arr: Array<Int>,
+) {
+    // 1-base SegmentTree
+    private val tree = Array(n * 4) { 0 }
+
+    private val n
+        get() = arr.size
+
+    init {
+        build(1, 0, n - 1)
+    }
+
+    private fun build(
+        node: Int,
+        start: Int,
+        end: Int
+    ) {
+        if (start == end) {
+            //
+            tree[node] = arr[start]
+        } else {
+            val mid: Int = (start + end) / 2
+            build(node * 2, start, mid)
+            build(node * 2 + 1, mid + 1, end)
+            tree[node] = tree[node * 2] + tree[node * 2 + 1]
+        }
+    }
+
+    fun query(
+        left: Int,
+        right: Int,
+        node: Int = 1,
+        start: Int = 0,
+        end: Int = n - 1,
+    ): Int {
+        if (right < start || left > end) {
+            // 해당 노드가 범위 밖이라면 계산하지 않는다.
+            return 0
+        } else if (left <= start && end <= right) {
+            // 해당 구간이 구해야하는 범위 안이면 전체 값을 리턴한다
+            return tree[node]
+        } else {
+            // left, right가 전체 배열의 범위에 애매하게 걸쳐 있는경우 쪼갠다.
+            val mid: Int = (start + end) / 2
+            val leftValue = query(
+                left = left,
+                right = right,
+                node = node * 2,
+                start = start,
+                end = mid
+            )
+            val rightValue = query(
+                left = left,
+                right = right,
+                node = node * 2 + 1,
+                start = mid + 1,
+                end = end
+            )
+
+            return leftValue + rightValue
+        }
+    }
+
+    fun update(
+        index: Int,
+        value: Int,
+        node: Int = 1,
+        start: Int = 0,
+        end: Int = n - 1,
+    ) {
+        if (start == end) {
+            // 리프 노드면 업데이트
+            tree[node] = value
+        } else {
+            val mid: Int = (start + end) / 2
+            // 바이너리 서치로 바꿀 위치를 찾아간다.
+            if (index <= mid) {
+                update(index, value, node * 2, start, mid)
+            } else {
+                update(index, value, node * 2 + 1, mid + 1, end)
+            }
+            tree[node] = tree[node * 2] + tree[node * 2 + 1]
+        }
+    }
+
+}
+
+// fun main() {
+
+//     val arr: Array<Int> = arrayOf(1, 2, 3, 4, 5)
+
+//     val segmentTree: SegmentTree = SegmentTree(arr)
+
+//     segmentTree.update(3, 6)
+//     print(segmentTree.query(arr.indexOf(2), arr.indexOf(5)))
+// }
